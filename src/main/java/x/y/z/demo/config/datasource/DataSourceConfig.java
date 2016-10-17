54a88da1acb857185fd8c84800d0bec4fba8f45b
Bogdan Ardeleanu <bogdan.ardeleanu@uti.ro>
2016-10-17 16:22:54 +0300

    ddl h2 Embeddable

--- src/main/java/x/y/z/demo/config/datasource/DataSourceConfig.java
+++ src/main/java/x/y/z/demo/config/datasource/DataSourceConfig.java
@@ -1,10 +1,8 @@
 package x.y.z.demo.config.datasource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
